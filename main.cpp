be171f8896753b052160f95bfc352209f5988bed
ericmelvin <bshweet@gmail.com>
2020-10-11 14:17:25 -0400

    bimodal now has functions for 1 and 2 bit counters

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <map>
 #include <math.h>
+#include <string>
 using namespace std;
 
 // global variables
@@ -15,7 +16,7 @@ int total_branches;
 struct input_data {
     unsigned long long pc;
     unsigned long long target;
-    uint8_t is_taken;
+    int is_taken;
 };
 
 // Prototypes
@@ -24,32 +25,68 @@ int always_taken(const vector<input_data> &fd);
 int always_not_taken(const vector<input_data> &fd);
 void output_results(ofstream &fout, vector<int> correct_output);
 void set_total_branches(int tb);
-int adjust_counter_1_bit(uint8_t is_taken, int counter);
-int bimodal_1_bit(const vector<input_data> &fd);
+int adjust_counter(int bit_len, int is_taken, int counter);
+int bimodal(int counter_limit, unsigned long long table_len, const vector<input_data> &fd);
 int main(void) {
+    // Vector of struct for input file data
     vector<input_data> fd;
+    // vector of int to hold all correct results
+    vector<int> correct_output;
     ifstream fin;
     ofstream fout;
-    vector<int> correct_output;
+
+    // Open input file and output file
     fin.open("../project1/traces/short_trace1.txt");
     fout.open("output.txt");
+
+    // Input file data to fd vector
     fd = read_file(fin, fd);
+
+    // Set global total_branches
     set_total_branches(fd.size());
     
+    // Run prediction algorithms
     correct_output.push_back(always_taken(fd));
+
     correct_output.push_back(always_not_taken(fd));
-    correct_output.push_back(bimodal_1_bit(fd));
+    
+    correct_output.push_back(bimodal(1, 16, fd));
+    correct_output.push_back(bimodal(1, 32, fd));
+    correct_output.push_back(bimodal(1, 128, fd));
+    correct_output.push_back(bimodal(1, 256, fd));
+    correct_output.push_back(bimodal(1, 512, fd));
+    correct_output.push_back(bimodal(1, 1024, fd));
+    correct_output.push_back(bimodal(1, 2048, fd));
+
+    correct_output.push_back(bimodal(3, 16, fd));
+    correct_output.push_back(bimodal(3, 32, fd));
+    correct_output.push_back(bimodal(3, 128, fd));
+    correct_output.push_back(bimodal(3, 256, fd));
+    correct_output.push_back(bimodal(3, 512, fd));
+    correct_output.push_back(bimodal(3, 1024, fd));
+    correct_output.push_back(bimodal(3, 2048, fd));
+    correct_output.push_back(bimodal_2bit(16, fd));
+    correct_output.push_back(bimodal_2bit(32, fd));
+
+
+
+    // Output results to file
     output_results(fout, correct_output);
+
+    // Close input and output files
     fin.close();
     fout.close();
 }
 
+/*Function: read_file:
+    input: fin: input file
+    input: fd: struct to hold file data
+    output: struct of file data */
 vector<input_data> read_file(ifstream &fin, vector<input_data> fd){ 
-    // Temporary variables
     unsigned long long addr;
     unsigned long long target;
-    char behavior;
-    uint8_t is_taken;
+    string behavior;
+    int is_taken;
     string line;
 
     // The following loop will read a line at a time
@@ -57,15 +94,22 @@ vector<input_data> read_file(ifstream &fin, vector<input_data> fd){
         // Now we have to parse the line into it's two pieces
         stringstream s(line);
         s >> std::hex >> addr >> behavior >> std::hex >> target;
-        // Add data to struct
-        is_taken = (behavior == 'T') ? 1 : 0;
+        
+        // Convert branch to 0 or 1
+        is_taken = (behavior == "T") ? 1 : 0;
+
+        // Add file data to struct
         input_data id = {addr, target, is_taken};
+
         // Add struct to vector
         fd.push_back(id);
     }
     return fd;
 }
 
+/*Function: always taken:
+    input: fd: struct of file data
+    output: number of correct branches predicted */
 int always_taken(const vector<input_data> &fd) {
     int correct = 0;
     for (int i=0; i<fd.size(); i++) {
@@ -76,6 +120,9 @@ int always_taken(const vector<input_data> &fd) {
     return correct;
 }
 
+/*Function: always not taken:
+    input: fd: struct of file data
+    output: number of correct branches predicted */
 int always_not_taken(const vector<input_data> &fd) {
     int correct = 0;
     for (int i=0; i<fd.size(); i++) {
@@ -86,59 +133,81 @@ int always_not_taken(const vector<input_data> &fd) {
     return correct;
 }
 
-int bimodal_1_bit(const vector<input_data> &fd) {
-    map<unsigned long long, uint8_t> lookup_table;
-    map<unsigned long long, uint8_t>::iterator iter;
-    uint8_t counter = 1;  // taken
+/*Function: bimodal:
+    input: counter_limit: the length of bits for the counter
+    input: table_len: the number of entries allowed in the lookup table
+    input: fd: struct of file data
+    output: number of correct branches predicted 
+    NOTE: initial counter value = counter_limit */
+int bimodal(int counter_limit, unsigned long long table_len, const vector<input_data> &fd) {
+    map<unsigned long long, int> lookup_table;
+    map<unsigned long long, int>::iterator iter;
+
+    
+
     int correct = 0;
+
     unsigned long long pc;
     unsigned long long mask;
     for (int i=0; i<fd.size(); i++) {
-        mask = pow(2, 4) -1;
-        pc = fd[i].pc & mask;
-        iter = lookup_table.find(pc);
-        if (iter == lookup_table.end()) { // pc doesn't exist in lookup table
-            lookup_table.insert(pair<int, int>(pc, counter));
+        mask = table_len-1;             // Create mask for pc based on table length
+        pc = fd[i].pc & mask;           // Obtain pc's lsb's
+        iter = lookup_table.find(pc);  // Does pc exist in table?
+        if (iter == lookup_table.end()) { // If pc does not exist
+            // Add pc to table with counter value
+            lookup_table.insert(pair<int, int>(pc, counter_limit));
         }
-        if (lookup_table[pc] == 1 && fd[i].is_taken) {
-            correct++; 
-        } else if (lookup_table[pc] == 0 && !fd[i].is_taken) {
-            correct++;
+
+        // Count correct predictions
+        if(counter_limit == 1) {
+                // If prediction is taken and actual outcome is taken
+            if (lookup_table[pc] > 0 && fd[i].is_taken) {
+                // Increment correct
+                correct++; 
+            } else if (lookup_table[pc] < 1 && !fd[i].is_taken) {  // not taken and actual outcome is not taken
+                // Increment correct
+                correct++;
+            }
+        } else if(counter_limit == 3) {
+            if (lookup_table[pc] > 1 && fd[i].is_taken) {
+                // Increment correct
+                correct++; 
+            } else if (lookup_table[pc] < 2 && !fd[i].is_taken) {  // not taken and actual outcome is not taken
+                // Increment correct
+                correct++;
+            }
+        } else {
+            cout << "Error: counter_limit must be 1 or 3" << endl;
+        }  
+
+        // Increment counter or decrement counter
+        if (fd[i].is_taken) { 
+            lookup_table[pc] = lookup_table[pc] + 1;
+            lookup_table[pc] = min(counter_limit, lookup_table[pc]); // Actual outcome adjusts counter
+            
+        } else if (!(fd[i].is_taken)) { 
+            lookup_table[pc] = lookup_table[pc] -1;
+            lookup_table[pc] = max(0, lookup_table[pc]);
+        } else {
+            cout << "ERROR: no known is_taken value" << endl;
         }
-        counter = adjust_counter_1_bit(fd[i].is_taken, counter);
-        lookup_table[pc] = counter;
-    }
-    for(auto elem : lookup_table) {
-        cout << elem.first << " " << elem.second << endl;
     }
     return correct;
-    
 }
-int adjust_counter_1_bit(uint8_t is_taken, int counter) {
-    if (counter > 1) { counter = 1; };
-    if (counter < 0) { counter = 0; };
 
-    if (is_taken && counter < 1) { 
-        return counter + 1; 
-    }
-    if (!is_taken && counter > 0) {
-        return counter - 1; 
-    }
-    
-    return counter;
-}
 void output_results(ofstream &fout, vector<int> correct_output){
     int result_count = correct_output.size();
 
     int i;
     vector<int> end_line = {0, 1, 8, 15, 24, 25, 26};
     for(i=0; i<result_count; i++){
-        fout << correct_output[i] << "," << total_branches << " ";
+        fout << correct_output[i] << "," << total_branches;
 
 
         if(count(end_line.begin(), end_line.end(), i)){
-            fout<< endl;
-            
+            fout << ";" << endl;
+        } else {
+            fout << "; ";
         }
     }
 }
